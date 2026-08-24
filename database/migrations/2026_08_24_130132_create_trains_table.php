32182d900ccdd8af1812e23ded93ab9fe1631108
andrea8980 <andrea@casaloda.it>
2026-08-24 15:07:34 +0200

    feat: add migration for creating trains table

---
+++ database/migrations/2026_08_24_130132_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda', 100);
+            $table->string('Stazione_di_partenza', 100);
+            $table->string('Stazione_di_arrivo', 100);
+            $table->time('Orario_di_partenza');
+            $table->time('Orario_di_arrivo');
+            $table->string('Codice_Treno', 100);
+            $table->tinyInteger('Numero_Carrozze');
+            $table->boolean('In_orario');
+            $table->boolean('Cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
