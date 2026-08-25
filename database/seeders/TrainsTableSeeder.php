1e5e7582ffe74865353afa29132d1a4571bae920
andrea8980 <andrea@casaloda.it>
2026-08-25 20:50:16 +0200

    feat: add Faker import to TrainsTableSeeder for data generation

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,24 +5,29 @@ use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Faker\Generator as Faker;
+
 class TrainsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
+
     {
-        $newTrain = new Train();
-        $newTrain->Azienda = 'Trenitalia';
-        $newTrain->Stazione_di_partenza = 'Roma Termini';
-        $newTrain->Stazione_di_arrivo = 'Milano Centrale';
-        $newTrain->Data_di_partenza = '2026-08-27 08:00:00';
-        $newTrain->Orario_di_partenza = '08:00:00';
-        $newTrain->Orario_di_arrivo = '12:00:00';
-        $newTrain->Codice_Treno = 'T123';
-        $newTrain->Numero_Carrozze = 10;
-        $newTrain->In_orario = true;
-        $newTrain->Cancellato = false;
-        $newTrain->save();
+    for($i=0; $i<100; $i++) {
+    $newTrain = new Train();
+    $newTrain->Azienda = $faker->company();
+    $newTrain->Stazione_di_partenza = $faker->city();
+    $newTrain->Stazione_di_arrivo = $faker->city();
+    $newTrain->Data_di_partenza = $faker->date();
+    $newTrain->Orario_di_partenza = $faker->time();
+    $newTrain->Orario_di_arrivo = $faker->time();
+    $newTrain->Codice_Treno = $faker->bothify('??###');
+    $newTrain->Numero_Carrozze = $faker->numberBetween(1, 20);
+    $newTrain->In_orario = $faker->boolean();
+    $newTrain->Cancellato = $faker->boolean();
+    $newTrain->save();
+        }
     }
 }
